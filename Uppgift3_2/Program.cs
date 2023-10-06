aa956b414141ae10635391f91672d8c05c22d56d
Keli06001 <keli06001@utb.vaxjo.se>
2023-10-06 12:58:11 +0200

    Add project files.

---
+++ Uppgift3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.ComponentModel.Design;
+
+namespace uppgift3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet, skriva j för ja eller n för nej");
+            string svar=Console.ReadLine().ToUpper();
+            Console.WriteLine("Hur gammal är du");
+            int gammal=int.Parse(Console.ReadLine());
+            if(svar == "J" &&  gammal < 22) 
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+            Console.ReadKey();
+        }
+    }
+}
